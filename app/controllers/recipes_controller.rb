81318add44d7d8f1ca70863f46354a2e5ad85732
Yury Voloshin <yvoloshin01@gmail.com>
2015-08-29 23:28:13 +0000

    Added recipe show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,19 @@ class RecipesController < ApplicationController
 		@recipe = Recipe.new
 	end
 
+	def create
+    Recipe.create(recipe_params)
+    redirect_to root_path
+  end
+
+  def show
+  	@recipe=Recipe.find(params[:id])
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :ingredients, :instructions)
+  end
+
 end
