ca206ee431e72638ecbf61b8ad47f656aff0058a
Yury Voloshin <yvoloshin01@gmail.com>
2015-09-25 05:40:05 +0000

    Added controller tests with fixtures

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,20 +1,28 @@
 class Recipe < ActiveRecord::Base
-	belongs_to :user
-	has_many :comments
-	validates :name, :presence => true, length: { minimum: 3 }
-	validates :ingredients, :presence => true
-	validates :instructions, :presence => true
+  belongs_to :user
+  has_many :comments
+  validates :name, :presence => true, length: { minimum: 3 }
+  validates :ingredients, :presence => true
+  validates :instructions, :presence => true
 
-
-	# It returns the places whose names contain one or more words that form the query
+  # Implements search for recipes.
+  # * *Args*    :
+  #   - search query in string format
+  # * *Returns* :
+  #   - returns the recipes with names that contain one or more words from the query
   def self.search(query)
-     #where(:title, query) -> This would return an exact match of the query
-    where("LOWER(name) like ?", "%#{query}%") 
+    #where(:title, query) -> This would return an exact match of the query
+    where("LOWER(name) like ?", "%#{query}%")
   end
 
+  # Calculates the average rating of recipe
+  # * *Args*    :
+  #   None
+  # * *Returns* :
+  #   - average rating of recipe
   def avg_rating
     rating_vals = self.comments.map do |c|
-        c.rating[0].to_i
+      c.rating[0].to_i
     end
 
     rating_vals.sum.to_f / rating_vals.length
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,59 @@
 require 'test_helper'
+# using fixtures
 
 class RecipesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	# test "recipe show page" do
+	# 	user = User.create(email: users(:one).email)
+	# 	sign_in user
+	# 	recipe = Recipe.create(name: recipes(:one).name,
+	# 													ingredients: recipes(:one).ingredients,
+	# 													instructions: recipes(:one).instructions,
+	# 													user: recipes(:one).user)
+	#   get :show, :id => recipe.id
+	# assert_response :success
+	#  end
+
+	test "create recipe" do
+
+		user = users(:one)		
+		sign_in user
+
+		# assert_difference 'user.recipes.count' do
+		# 	puts user.recipes.count
+	 #    post :create, {:recipe => {
+	 #        :name => 'test2', # recipes(:two).name,
+	 #        :ingredients => 'ingredient1, ingredient2', # recipes(:two).ingredients,
+	 #        :instructions => 'instruction1, instruction2', # recipes(:two).instructions,
+	 #        :user => user
+	 #      }
+	 #    }
+	 #  end
+
+	 		puts user.recipes.count
+	 		puts Recipe.count
+
+	    # recipe = Recipe.create(name: recipes(:two).name,
+	 			# 										ingredients: recipes(:two).ingredients,
+	 			# 										instructions: recipes(:two).instructions)
+		
+	    post :create, {:recipe => {
+	        :name => 'test2',
+	        :ingredients => 'ingredient1, ingredient2', 
+	        :instructions => 'instruction1, instruction2' 
+	      }
+	    }
+
+	    # post :create, { :recipe => recipes(:one) }
+
+	    puts user.recipes.count
+	    puts Recipe.count
+	  
+		# assert_redirected_to root_path
+	  
+	  assert_equal 1, user.recipes.count
+
+	end
+
+
 end
