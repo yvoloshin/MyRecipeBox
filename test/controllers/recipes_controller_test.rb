bcda1121f41ddc6605ec55cfaadf8eddac76469e
Yury Voloshin <yvoloshin01@gmail.com>
2015-09-25 05:49:05 +0000

    Modified controller tests

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -3,39 +3,22 @@ require 'test_helper'
 
 class RecipesControllerTest < ActionController::TestCase
 
-	# test "recipe show page" do
-	# 	user = User.create(email: users(:one).email)
-	# 	sign_in user
-	# 	recipe = Recipe.create(name: recipes(:one).name,
-	# 													ingredients: recipes(:one).ingredients,
-	# 													instructions: recipes(:one).instructions,
-	# 													user: recipes(:one).user)
-	#   get :show, :id => recipe.id
-	# assert_response :success
-	#  end
+	test "recipe show page" do
+		user = users(:one)
+		sign_in user
+		recipe = Recipe.create(name: recipes(:one).name,
+														ingredients: recipes(:one).ingredients,
+														instructions: recipes(:one).instructions,
+														user: recipes(:one).user)
+	  get :show, :id => recipe.id
+	  assert_response :success
+	end
 
 	test "create recipe" do
 
 		user = users(:one)		
 		sign_in user
 
-		# assert_difference 'user.recipes.count' do
-		# 	puts user.recipes.count
-	 #    post :create, {:recipe => {
-	 #        :name => 'test2', # recipes(:two).name,
-	 #        :ingredients => 'ingredient1, ingredient2', # recipes(:two).ingredients,
-	 #        :instructions => 'instruction1, instruction2', # recipes(:two).instructions,
-	 #        :user => user
-	 #      }
-	 #    }
-	 #  end
-
-	 		puts user.recipes.count
-	 		puts Recipe.count
-
-	    # recipe = Recipe.create(name: recipes(:two).name,
-	 			# 										ingredients: recipes(:two).ingredients,
-	 			# 										instructions: recipes(:two).instructions)
 		assert_difference 'user.recipes.count' do
 	    post :create, {:recipe => {
 	        :name => 'test2',
@@ -44,13 +27,8 @@ class RecipesControllerTest < ActionController::TestCase
 	      }
 	    }
 	  end
-
-	    # post :create, { :recipe => recipes(:one) }
-
-	    puts user.recipes.count
-	    puts Recipe.count
 	  
-		# assert_redirected_to root_path
+	  assert_redirected_to root_path
 	  
 	  assert_equal 1, user.recipes.count
 
